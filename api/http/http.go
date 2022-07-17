514711f57f16c637151b09c3379b2c9852050bf8
vugiahung.uet <vugiahung.uet@gmail.com>
2022-07-17 23:10:27 +0700

    add http server

--- api/http/http.go
+++ api/http/http.go
@@ -1,11 +1,15 @@
 package http
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"oneclick/config"
 	"os"
 	"os/signal"
 	"regexp"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/form/v4"
@@ -29,7 +33,6 @@ type Server struct {
 	httpServer *http.Server
 	services   map[string]interface{}
 }
-
 
 func NewServer() (*Server, error) {
 	var corsEnable bool
@@ -78,4 +81,37 @@ func (s *Server) Start() {
 	}
 	s.l.Infof("Server gracefully stopped")
 }
+
+type errorResponse struct {
+	Timestamp int64  `json:"timestamp"`
+	Error     string `json:"error"`
+}
+
+func parseJSON(input interface{}, c *gin.Context) error {
+	defer c.Request.Body.Close()
+	contentType := c.ContentType()
+	switch contentType {
+	case "application/json":
+		decoder := json.NewDecoder(c.Request.Body)
+		return decoder.Decode(input)
+	default:
+		return fmt.Errorf("Unspported contentType type: %s", contentType)
+	}
+}
+
+func normalizeMethod(method string) string {
+	if len(method) == 0 {
+		return method
+	}
+	return strings.ToUpper(method[0:1]) + method[1:]
+}
+
+func getParamMap(params gin.Params) map[string][]string {
+	res := make(map[string][]string)
+	for _, p := range params {
+		res[p.Key] = []string{p.Value}
+	}
+	return res
+}
+
 
